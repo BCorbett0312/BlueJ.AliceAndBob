bb6b67328906638605bc504113e73b1e58bdb05d
Benjamin Corbett <benjamincorbett@zipcodersMBP37.localdomain>
2019-05-21 17:23:17 -0400

    Alice Bob

--- Main.java
+++ Main.java
@@ -1,9 +1,23 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args ){
-
+        Scanner in = new Scanner(System.in);
+        
+        System.out.print("What is your name? ");
+        String name = in.next();
+        
+        //System.out.print(name);
+        
+        
+        
+        if(name.equalsIgnoreCase("Bob")) {System.out.print("Hello " + name);}
+        else if(name.equalsIgnoreCase("alice")){System.out.print("Hello " + name);}
+        else {System.out.print("Goodbye");}
     }
 }
